2445cb98a2d72662cbd81a6b920a66208577db44
aklry <3265242862@qq.com>
2021-11-21 14:12:21 +0800

    chore(package.json): 添加commitizen

--- src/main.js
+++ src/main.js
@@ -1,19 +1,31 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import "normalize.css/normalize.css"; // A modern alternative to CSS resets
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/en"; // lang i18n
 
-import '@/styles/index.scss' // global css
+Vue.directive("anti-shake", {
+  inserted: function (el, binding) {
+    let timer = null;
+    el.addEventListener("keypress", () => {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        binding.value();
+      }, 300);
+    });
+  },
+});
 
-import App from './App'
-import store from './store'
-import router from './router'
+import "@/styles/index.scss"; // global css
 
-import '@/icons' // icon
-import '@/permission' // permission control
+import App from "./App";
+import store from "./store";
+import router from "./router";
+
+import "@/icons"; // icon
+import "@/permission"; // permission control
 
 /**
  * If you don't want to use mock-server
@@ -23,21 +35,21 @@ import '@/permission' // permission control
  * Currently MockJs will be used in the production environment,
  * please remove it before going online ! ! !
  */
-if (process.env.NODE_ENV === 'production') {
-  const { mockXHR } = require('../mock')
-  mockXHR()
+if (process.env.NODE_ENV === "production") {
+  const { mockXHR } = require("../mock");
+  mockXHR();
 }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, { locale });
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
-  render: h => h(App)
-})
+  render: (h) => h(App),
+});
---
+++ src/views/directive/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dialog">
+      <input v-model="value" v-anti-shake="getData" type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Dialog',
+    data() {
+        return {
+            value: ''
+        }
+    },
+    mounted(){},
+    methods: {
+        getData(value) {
+            console.log(value)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
