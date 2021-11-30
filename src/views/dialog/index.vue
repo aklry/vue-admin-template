019f58f0f01d486653dbe088e6ed7751d1f9cf9b
Aklry <74409739+aklry@users.noreply.github.com>
2021-11-30 12:59:42 +0800

    feat(views/dislog): 全局弹窗

--- src/views/dialog/index.vue
+++ src/views/dialog/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div />
+  <div class="message">
+    <el-button @click="open">打开消息框</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Message1',
+  mounted() {
+  },
+  methods: {
+    open() {
+      this.$Message({
+        title: '我是消息框',
+        bgColor: 'red'
+      }, {
+        btn: <el-button type='success'>按钮</el-button>
+      }).init()
+    }
+  }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
