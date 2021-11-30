00634ea416c2a818382b353d9eb9d29211206a16
Aklry <74409739+aklry@users.noreply.github.com>
2021-11-30 13:04:50 +0800

    feat(main.js): 全局注册

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import Index from '@/views/dialog/file/index.js'
 
+Vue.use(Index)
 Vue.directive('anti-shake', {
   inserted: function(el, binding) {
     let timer = null
